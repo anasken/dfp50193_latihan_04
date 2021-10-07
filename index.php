c578e8b502afb5cce1cda414b3be3f9e07dfb98e
anasken <anashalim01@gmail.com>
2021-10-07 08:21:08 +0800

    Create index.php

---
+++ index.php
@@ -0,0 +1,19 @@
+<?php
+//$jawatan = 'Pengurus';
+define('JAWATAN', 'Pengurus');
+?>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Constants</title>
+</head>
+<body>
+    <p>
+    Say selaku <? echo JAWATAN; ?>
+    dengan ini melantik Muhammad Anas sebagai Penolong <? echo JAWATAN; ?>
+    </p>
+</body>
+</html>
